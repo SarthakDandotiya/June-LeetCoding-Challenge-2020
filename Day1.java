a940182bdca414eb2af8b370ce14a5342552152d
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-06-01 18:19:43 +0530

    Day 1 Solution

---
+++ Day1.java
@@ -0,0 +1,25 @@
+// Question:
+// Invert Binary Tree
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // switch left and right node at each level
+
+        if (root == null)
+            return root;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
