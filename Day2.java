515f5884214f7c7541a24a5475ae5651a88bddfa
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-06-03 00:47:59 +0530

    Day 2 Solution

---
+++ Day2.java
@@ -0,0 +1,15 @@
+// Question:
+// Delete Node in a Linked List
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        // take given node and point it to next node while updating value too
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
