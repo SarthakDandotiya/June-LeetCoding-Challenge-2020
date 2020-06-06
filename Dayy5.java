b856cbd2d437a94b6ab49fc5bb6a8bde7ff2a008
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-06-06 11:40:47 +0530

    Day 5 Solution

---
+++ Dayy5.java
@@ -0,0 +1,28 @@
+// Question:
+// Random Pick with Weight
+
+class Solution {
+    private double[] probabilities;
+
+    public Solution(int[] w) {
+        double sum = 0;
+        this.probabilities = new double[w.length];
+        for (int weight : w)
+            sum += weight;
+        for (int i = 0; i < w.length; i++) {
+            w[i] += (i == 0) ? 0 : w[i - 1];
+            probabilities[i] = w[i] / sum;
+        }
+    }
+
+    public int pickIndex() {
+        return Math.abs(Arrays.binarySearch(this.probabilities, Math.random())) - 1;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such: Solution obj =
+ * new Solution(w); int param_1 = obj.pickIndex();
+ */
+
+// Couldn't Understand Question, took help...
