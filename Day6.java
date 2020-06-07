2ca2a2941daabe21c39bc8a8404df9cbb78a54fe
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-06-07 12:21:37 +0530

    Day 6 Solution

---
+++ Day6.java
@@ -0,0 +1,34 @@
+// Question:
+// Queue Reconstruction by Height
+
+class CustomComparator implements Comparator<int[]> {
+    public int compare(int[] a1, int[] a2) {
+        if (a1[0] != a2[0]) {
+            return a2[0] - a1[0];
+        } else {
+            return a1[1] - a2[1];
+        }
+    }
+}
+
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        int[][] result = new int[people.length][];
+        Arrays.sort(people, new CustomComparator());
+
+        ArrayList<int[]> list = new ArrayList<int[]>();
+
+        for (int i = 0; i < people.length; i++) {
+            int[] arr = people[i];
+            list.add(arr[1], arr);
+        }
+
+        for (int i = 0; i < people.length; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
+
+// Syntax Help taken
