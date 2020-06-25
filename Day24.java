4e8f75c36380190890df901eb0dbd8ea37fcb272
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-06-25 07:57:51 +0530

    Day 24 Solution

---
+++ Day24.java
@@ -0,0 +1,20 @@
+// Question: 
+// Unique Binary Search Trees
+// .*
+
+class Solution {
+    public int numTrees(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += (dp[j] * dp[i - j - 1]);
+            }
+        }
+
+        return dp[n];
+    }
+}
